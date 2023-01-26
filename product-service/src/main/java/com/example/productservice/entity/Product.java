50af29ad8d4f316014ed28a2f04959eb6e021c44
aviNelson <valera.zz73@gmail.com>
2023-01-26 18:37:54 +0300

    Rename module eureka-server

--- product-service/src/main/java/com/example/productservice/entity/Product.java
+++ product-service/src/main/java/com/example/productservice/entity/Product.java
@@ -13,7 +13,6 @@ import java.util.Objects;
 @Builder
 @Getter
 @Setter
-@ToString
 @Table(name = "product")
 public class Product {
 
@@ -27,17 +26,4 @@ public class Product {
 
     private BigDecimal price;
 
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        Product product = (Product) o;
-        return id != null && Objects.equals(id, product.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
 }
